fa6f6c85f9de240e27fd0294192691400700cc3c
fabianaColombo <fabicriscolombo@gmail.com>
2023-01-07 22:24:38 +0100

    postgress database configuration: environment configuration connection configuration

---
+++ config/db.config.js
@@ -0,0 +1,31 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+
+const connect = () => {
+  const { database, username, password, hostname, dialect } = config[env];
+
+  const sequelize = new Sequelize(database, username, password, {
+    host: hostname,
+    dialect: dialect,
+    operatorsAliases: false,
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 20000,
+      idle: 5000,
+    },
+  });
+
+  const db = {};
+  db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
+  db.orders = require("../models/order")(sequelize, DataTypes, Model);
+  db.users = require("../models/user")(sequelize, DataTypes, Model);
+  db.products = require("../models/product")(sequelize, DataTypes, Model);
+
+  return db;
+};
+
+module.exports = {
+  connect,
+};
